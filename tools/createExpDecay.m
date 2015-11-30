3fb3fd284b737ea462c5d168db534621b4f2d5b7
Joshua Arnold <joshua.arnold1@uqconnect.edu.au>
2015-12-01 06:56:54 +1000

    expDecay at first network running time

--- tools/createExpDecay.m
+++ tools/createExpDecay.m
@@ -1,17 +1,18 @@
-function [ ] = createExpDecay( filename, outDir, evtsPimg, k )
+function [ ] = createExpDecay( filename, outDir, msin, kin )
 %CREATEEXPDECAY Convert a given aedat file to a data set with exponential decay
 %   Open the given aedat file and apply decay with the given parameters
-%   saving an image of the decay to the out
-%   Example: createExpDecay('ball.aedat', 'play', 5000, 0.5)
+%   saving an image of the decay to the out. msin is milliseconds in
+%   Example: createExpDecay('ball.aedat', 'play', 30, 0.5)
 
-
+    disp('start loading aedat file');
     [allAddr, allTs] = loadaerdat(filename);
     %extracts all the information from the address matrices in the form
     %[xcoordinate, ycoordinate, polarity]
     [infomatrix1, infomatrix2, infomatrix3] = extractRetina128EventsFromAddr(allAddr);
     %times are in us
     allTs = int32(allTs);
-    k = k * 5e5;  % 1 second is 1e6
+    k = kin * 5e5;  % 1 second is 1e6 so just do half second (5e5)
+    timesliceus = msin * 1e3;
     
     DVS_RESOLUTION = 128;
     total_spikes = size(allTs);
@@ -22,35 +23,70 @@ function [ ] = createExpDecay( filename, outDir, evtsPimg, k )
     cur_spike = 1;
     lastSave = 1;
     count = 1;
-    figure
-    while 1 == 0
-    %while cur_spike < total_spikes
+
+    % Write meta data to text file
+    fileID = fopen(sprintf('%s/readme.txt', outDir), 'w');
+    fprintf(fileID, 'Exponential Decay: filename outdir eventsPerImage k\n');
+    fprintf(fileID, '%s %s %d %d\n', filename, outDir, msin, kin);
+    fclose(fileID);
+    
+    % process data
+%     disp('start pre-process');
+%     spike = 1;
+%     freq = 0;
+%     last_check = 1;
+%     nallTs = [];
+%     while spike < size(allTs, 1)
+%         if allTs(spike) - allTs(last_check)
+%             
+%         end
+%         spike = spike + 1;
+%     end
+    
+    
+    
+    
+    %DEBUG: while 1 == 0
+    disp('start forward pass (into past)');
+    freq = 0;
+    while cur_spike < total_spikes
         x = infomatrix1(cur_spike)+1; %offset as DVS numbers from 0
         y = infomatrix2(cur_spike)+1;
         %pol = infomatrix3(cur_spike); %unused atm
         time = allTs(cur_spike);
+        cur_spike = cur_spike + 1;
         
         lastSpikeTimes(x, y) = time;
-        cur_spike = cur_spike + 1;
+        
+        
         %disp(cur_spike - lastSave);
-        if cur_spike - lastSave > evtsPimg
+        if allTs(cur_spike) - allTs(lastSave) > timesliceus
+            
             timeDif = time - lastSpikeTimes;
+            
+       
+            % THIS IS EXP DECAY
             res = 1./(1 + exp(-double(timeDif)./k));
             
-            outfile = sprintf('%s/%s_%devts_%05d_fwds.png', ...
-                    outDir, filename, evtsPimg, count);
             
-            imshow(flipud(res.'), 'InitialMagnification', 300)%, outfile);
+            
+            outfile = sprintf('%s/%s_%dms_%05d_past.png', ...
+                    outDir, filename, msin, count);
+            
+            imwrite(flipud(res.'), outfile); % 'InitialMagnification', 300
             count = count + 1;
             lastSave = cur_spike;
         end
     end
-    cur_spike = size(allTs, 1);
-    count = 1000;
-    disp('BACKWARDS');
+    
+    
+    %DEBUG: cur_spike = size(allTs, 1);
+    %DEBUG: count = 1000;
+    disp('Start backwards pass (into future)');
     % cur_spike, count, are at the end
     lastSpikeTimes = int32(zeros(DVS_RESOLUTION, DVS_RESOLUTION));
-    lastSave = cur_spike; % in case some events were left out from fwds
+    lastSave = timesliceus + lastSave;%cur_spike; % in case some events were left out from fwds
+    %cur_spike = lastSave
     % Now we go backwards
     while cur_spike > 1
         %disp('in');
@@ -58,20 +94,19 @@ function [ ] = createExpDecay( filename, outDir, evtsPimg, k )
         y = infomatrix2(cur_spike)+1;
         %pol = infomatrix3(cur_spike); %unused atm
         time = allTs(cur_spike);
-        
-        lastSpikeTimes(x, y) = time;
         cur_spike = cur_spike - 1;
+
+        lastSpikeTimes(x, y) = time;
+
         %disp(cur_spike - lastSave);
-        if lastSave - cur_spike > evtsPimg
-            %disp('inner if');
+        if lastSave > size(allTs,1) || allTs(lastSave) - allTs(cur_spike) > timesliceus
             timeDif = lastSpikeTimes - time;
             timeDif(timeDif == -time) = time;
             res = 1./(1 + exp(-double(timeDif)./k));
             
-            outfile = sprintf('%s/%s_%devts_%05d_bkwd.png', ...
-                    outDir, filename, evtsPimg, count);
-            
-            imshow(flipud(res.'), 'InitialMagnification', 300)%, outfile);
+            outfile = sprintf('%s/%s_%dms_%05d_futr.png', ...
+                    outDir, filename, msin, count);
+            imwrite(flipud(res.'), outfile); %'InitialMagnification', 300
             count = count - 1;
             lastSave = cur_spike;
         end
