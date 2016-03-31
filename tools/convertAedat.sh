16478611c1b5633b6a4b8981e35fb6201cd880d1
Joshua Arnold <joshua.arnold1@uqconnect.edu.au>
2016-03-31 10:49:08 +1000

    streamlined preprocessing

--- tools/convertAedat.sh
+++ tools/convertAedat.sh
@@ -6,6 +6,18 @@
 #   Find a way to sort data into seperate folders after processing so to be 
 #       completely ready to run the neural network.
 
+AEDATFILE='data/dvs_gt4.aedat'
+OUTFOLDER='gt4'
+MSIN='30'
+K='0.5'
 
-matlab -nodisplay -r 'runpreprocess, exit'
+# Delete output folder if it already exists and recreate
+if [ -d $OUTFOLDER ]; then
+    rm -rf $OUTFOLDER
+fi
 
+mkdir $OUTFOLDER
+mkdir $OUTFOLDER/data
+
+#matlab -nodisplay -r 'runpreprocess, exit'
+matlab -nosplash -nodisplay -r "preprocess('$AEDATFILE', '$OUTFOLDER', 'exponential', 'msin', $MSIN, 'k', $K), exit"
--- tools/runpreprocess.m
+++
@@ -1,8 +0,0 @@
-aedatFile = 'data/dvs_gt5.aedat';
-outputFolder = 'gt5';
-msin = 30;
-k = 0.5;
-
-
-mkdir(outputFolder);
-preprocess(aedatFile, outputFolder, 'exponential', 'msin', msin, 'k', k);
