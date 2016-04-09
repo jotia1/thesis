b22944548c458ebc50de9c6040af61b903010a06
Joshua Arnold <joshua.arnold1@uqconnect.edu.au>
2016-04-09 18:21:51 +1000

    Further visualisation support added

--- tf_models/net2.py
+++ tf_models/net2.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 import netTools
 import time
 import math
+from matplotlib import pyplot as plt
 
 PICKLE_FILE = 'gt5_20k.pickle'
 #TENSORBOARD_DIR = '/home/Student/s4290365/thesis/tf_models/tensorBoard'
@@ -18,8 +19,8 @@ IMAGE_SIZE = 128
 TOTAL_PIXELS = IMAGE_SIZE * IMAGE_SIZE
 
 BATCH_SIZE = 100
-TOTAL_STEPS = 501
-HIDDEN_UNITS = 64
+TOTAL_STEPS = 30001
+HIDDEN_UNITS = 16
 LEARNING_RATE = 0.5
 
 # Load and separate datasets
@@ -40,36 +41,71 @@ with tf.Graph().as_default():
     
     # Define hidden layer
     with tf.name_scope('hidden'):
-        weights = tf.Variable(
-            tf.truncated_normal([TOTAL_PIXELS, HIDDEN_UNITS], 
-                                stddev=1.0 / math.sqrt(float(TOTAL_PIXELS)),
-            name='weights'))
+        weights = tf.Variable(tf.truncated_normal([TOTAL_PIXELS, HIDDEN_UNITS], 
+                                stddev=1.0 / HIDDEN_UNITS),
+                                name='weights')
     
-        biases = tf.Variable(tf.zeros([HIDDEN_UNITS]),
+        biases = tf.Variable(tf.truncated_normal([HIDDEN_UNITS], 
+                                stddev=1.0 / HIDDEN_UNITS),
                                 name='biases')
         hidden = tf.nn.relu(tf.matmul(input_placeholder, weights) + biases)
 
-    tf.histogram_summary("hid_weights", weights)
+    #tf.image_summary("hid_weights", weights.reshape([1, TOTAL_PIXELS, HIDDEN_UNITS, 1]))
     tf.histogram_summary("hid_biases", biases)
 
     # Define output layer
     with tf.name_scope('out_layer'):
         weights = tf.Variable(
             tf.truncated_normal([HIDDEN_UNITS, TOTAL_PIXELS],
-                                stddev=1.0 / math.sqrt(float(TOTAL_PIXELS)),
-            name='weights'))
+                                stddev=1.0 / HIDDEN_UNITS),
+            name='weights')
 
-        biases = tf.Variable(tf.zeros([TOTAL_PIXELS]),
-                                name='biases')    
+        biases = tf.Variable(tf.truncated_normal([TOTAL_PIXELS], 
+                                stddev=1.0 / HIDDEN_UNITS),
+                                name='biases')
         logits = tf.matmul(hidden, weights) + biases
 
     # Could consider using reduce_sum and normalise
     # loss function - Sum squared difference (well mean...)
-    loss = tf.div(tf.reduce_sum(tf.square(label_placeholder - logits), name='loss'), TOTAL_PIXELS*BATCH_SIZE)
+    loss = tf.div(tf.reduce_sum(
+        tf.square(label_placeholder - logits)), 
+        TOTAL_PIXELS*BATCH_SIZE, name='loss')
     #loss = tf.reduce_sum(tf.square(label_placeholder - logits), name='loss')
 
+    """
+    with tf.name_scope('loss_layer'):
+        with tf.name_scope('lamb'):
+            # Computer L(actual) = actual * m + c
+            g = float(10) # Grayness (or whiteness) of the scene
+            m = tf.constant( (TOTAL_PIXELS - (2.0 * g)) / TOTAL_PIXELS, dtype=tf.float32 )
+            c = tf.constant( float(g) / TOTAL_PIXELS, dtype=tf.float32)
+            lamb = tf.mul(m, logits) + c
+        
+        squr = tf.square(label_placeholder - logits, name='squr')
+        tmp = tf.mul(squr, lamb, name='tmp')
+        loss = tf.reduce_sum(tmp)
+    """
+
+
+    """
+    with tf.name_scope('loss_layer'):
+        # 'LINEAR' LOSS
+        g = 10.0 # 'Grayness' of the screen
+        # l is a linear function of the penalty of a prediction
+        m = tf.constant((TOTAL_PIXELS - ( 2 * g ) ) / TOTAL_PIXELS, dtype=tf.float32)
+        c = tf.constant(g / TOTAL_PIXELS, dtype=tf.float32)
+        l = tf.mul(m, logits) + c 
+        loss = tf.reduce_sum(tf.mul(tf.square(label_placeholder - logits), l), name='loss')
+    """
+
+    #tf.scalar_summary('loss_m', m)
+    #tf.scalar_summary('loss_c', c)
+    #tf.histogram_summary('tmp', tmp)
+    #tf.histogram_summary('lamb', lamb)
+    #tf.histogram_summary('squr', squr)
+    #tf.scalar_summary('loss_l', l)
     # Log data
-    tf.image_summary("prediction", netTools.tensor2img(logits, BATCH_SIZE))
+    #tf.image_summary("prediction", netTools.tensor2img(logits, BATCH_SIZE))
     tf.scalar_summary(loss.op.name, loss)
 
     # Training operations
@@ -105,8 +141,20 @@ with tf.Graph().as_default():
                     label_placeholder : batch_labels 
                     }
 
+        """
+        ## DEBUGGING ################
+        # Just compute the loss
+        loss_value = sess.run([loss], feed_dict=feed_dict)
+        # THen write the loss and other stats to file
+        summary_str = sess.run(summary_op, feed_dict=feed_dict)
+        summary_writer.add_summary(summary_str, step)
+
+        continue  # Force loop restart
+        #################    END DEBUG   ################
+        """
+
         _, loss_value = sess.run([train_op, loss], feed_dict=feed_dict)
-    
+
         duration = time.time() - start_time
 
         if step % 100 == 0:
@@ -114,7 +162,7 @@ with tf.Graph().as_default():
             summary_writer.add_summary(summary_str, step)
             
             # Save a checkpoint and evaluate the model periodically.
-            if step % 1000 == 0:
+            if step % 300 == 0:
                 saver.save(sess, SAVE_DIR, global_step=step)
                 
                 # RUN Network with validation data
@@ -125,8 +173,13 @@ with tf.Graph().as_default():
                             label_placeholder : batch_labels 
                             }
 
-                validation_value = sess.run(loss, feed_dict=feed_dict)
+                preds, validation_value = sess.run([logits, loss], feed_dict=feed_dict)
                 print("Step: %d: validation: %.5f" % (step, validation_value))
+                
+                if step == 30000:
+                    # Save predictions for viewing later
+                    pic_name = "test" + str(step) 
+                    netTools.save_preds(batch_data, preds, pic_name, batch_size=BATCH_SIZE)
 
 
 
--- tf_models/netTools.py
+++ tf_models/netTools.py
@@ -1,6 +1,7 @@
 import cPickle as pickle
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 IMAGE_SIZE = 128
 
@@ -47,3 +48,94 @@ def load_pickle_dataset(pickleName):
 
     return (train_dataset, train_labels), (valid_dataset, valid_labels), (test_dataset, test_labels)
 
+
+def save_preds(intensor, predictions, filename, batch_size=100):
+    """ Given network input and predicton tensors save them to given file
+        Tensor should be in the form [BATCHSIZE, TOTAL_PIXELS]
+    """
+    np.savez(filename, ins=intensor, preds=predictions)
+
+
+    #netin = tensor2img(intensor, batch_size)
+    #preds = tensor2img(predictions, batch_size)
+    #f = open(picklename, 'wb')
+    #save = {
+    #    'netin': netin,
+    #    'preds': preds,
+    #}
+    #pickle.dump(save, f, pickle.HIGHEST_PROTOCOL)
+    #f.close()
+    #np.save(filename, save)
+    print("Saved", filename)
+
+def show_preds(filename):
+    """ Open a pickle file containing predictions and display them asking the
+        user for input
+    """
+    dataset = np.load(filename)
+    if 'ins' not in dataset.files or 'preds' not in dataset.files:
+        raise Exception("Dataset missing ins or preds, may not be valid")
+    ins = dataset['ins']
+    preds = dataset['preds']
+    assert ins.shape == preds.shape
+    
+    raise Exception("Function NOT Finished")
+
+    cur_img = 0        
+
+    while True:
+        usr = input("Action (n/b/q):")
+        if usr == 'n':
+            pass
+        elif usr == 'b':
+            pass
+        elif usr == 'q':
+            break
+        else:
+            print("Unreccognised command:", usr)
+
+def preds2png(filename, outdir):
+    """ Given a saved file convert in/prediction pairs to pngs and save to
+        outdir
+        Preconditon: Assumes outdir exists and is writeable
+                    filename is valid and readable
+    """
+     
+    dataset = np.load(filename)
+    if 'ins' not in dataset.files or 'preds' not in dataset.files:
+        raise Exception("Dataset missing ins or preds, may not be valid")
+    ins = dataset['ins']
+    preds = dataset['preds']
+    del dataset
+    assert ins.shape == preds.shape
+    
+    batch_size = ins.shape[0]
+    
+    for i in xrange(batch_size):
+        in_img = ins[i, :].reshape(IMAGE_SIZE, IMAGE_SIZE)
+        pred_img = preds[i, :].reshape(IMAGE_SIZE, IMAGE_SIZE)
+
+        plt.imshow(in_img, cmap='gray')  # Save input img
+        plt.savefig(outdir + str(i) + "in.png")
+    
+        plt.imshow(pred_img, cmap='gray')  # save prediction
+        plt.savefig(outdir + str(i) + "out.png")
+        
+        # When visualising weights can use something like
+        # http://stackoverflow.com/questions/11775354/how-can-i-display-a-np-array-with-pylab-imshow
+        # im = plt.imshow(arr, cmap='hot')
+        # plt.colorbar(im, orientation='horizontal')
+        # plt.show()        
+        
+        # multiple images in one figure
+        # http://stackoverflow.com/questions/17111525/how-to-show-multiple-images-in-one-figure        
+
+
+
+
+
+
+
+
+
+ 
