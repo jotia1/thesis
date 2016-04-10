Fixed numerical instability, sperated tools

tf_models/net2.py
@@ -3,13 +3,12 @@ import tensorflow as tf
 import netTools
 import time
 import math
-from matplotlib import pyplot as plt
 
 PICKLE_FILE = 'gt5_20k.pickle'
 #TENSORBOARD_DIR = '/home/Student/s4290365/thesis/tf_models/tensorBoard'
 TENSORBOARD_DIR = 'tensorBoard'
 #SAVE_DIR = '/home/Student/s4290365/thesis/tf_models/saveDir'
-SAVE_DIR = 'saveDir'
+SAVE_DIR = 'saveDir/'
 
 # CONSTANTS
 TRAIN_INDEX = 0
@@ -19,7 +18,7 @@ IMAGE_SIZE = 128
 TOTAL_PIXELS = IMAGE_SIZE * IMAGE_SIZE
 
 BATCH_SIZE = 100
-TOTAL_STEPS = 30001
+TOTAL_STEPS = 5001
 HIDDEN_UNITS = 16
 LEARNING_RATE = 0.5
 
@@ -52,8 +51,11 @@ with tf.Graph().as_default():
 
     #tf.image_summary("hid_weights", weights.reshape([1, TOTAL_PIXELS, HIDDEN_UNITS, 1]))
     tf.histogram_summary("hid_biases", biases)
+    tf.histogram_summary("hid_weights", weights)
+    tf.histogram_summary("hidden", hidden)
 
     # Define output layer
+    """
     with tf.name_scope('out_layer'):
         weights = tf.Variable(
             tf.truncated_normal([HIDDEN_UNITS, TOTAL_PIXELS],
@@ -64,48 +66,34 @@ with tf.Graph().as_default():
                                 stddev=1.0 / HIDDEN_UNITS),
                                 name='biases')
         logits = tf.matmul(hidden, weights) + biases
+    """
 
-    # Could consider using reduce_sum and normalise
+    #tf.histogram_summary("out_biases", biases)
+    #tf.histogram_summary("out_weights", weights)
+    #tf.histogram_summary("logits", logits)
     # loss function - Sum squared difference (well mean...)
-    loss = tf.div(tf.reduce_sum(
-        tf.square(label_placeholder - logits)), 
-        TOTAL_PIXELS*BATCH_SIZE, name='loss')
+    #loss = tf.div(tf.reduce_sum(
+    #    tf.square(label_placeholder - logits)), 
+    #    TOTAL_PIXELS*BATCH_SIZE, name='loss')
     #loss = tf.reduce_sum(tf.square(label_placeholder - logits), name='loss')
 
-    """
     with tf.name_scope('loss_layer'):
         with tf.name_scope('lamb'):
             # Computer L(actual) = actual * m + c
             g = float(10) # Grayness (or whiteness) of the scene
             m = tf.constant( (TOTAL_PIXELS - (2.0 * g)) / TOTAL_PIXELS, dtype=tf.float32 )
             c = tf.constant( float(g) / TOTAL_PIXELS, dtype=tf.float32)
-            lamb = tf.mul(m, logits) + c
+            lamb = tf.mul(m, hidden) + c
         
-        squr = tf.square(label_placeholder - logits, name='squr')
-        tmp = tf.mul(squr, lamb, name='tmp')
-        loss = tf.reduce_sum(tmp)
-    """
+        squr = tf.square(label_placeholder - hidden, name='squr')
+        tmp = tf.div(tf.mul(squr, lamb), BATCH_SIZE, name='tmp')
+        loss = tf.reduce_sum(tmp, name='loss')
 
 
-    """
-    with tf.name_scope('loss_layer'):
-        # 'LINEAR' LOSS
-        g = 10.0 # 'Grayness' of the screen
-        # l is a linear function of the penalty of a prediction
-        m = tf.constant((TOTAL_PIXELS - ( 2 * g ) ) / TOTAL_PIXELS, dtype=tf.float32)
-        c = tf.constant(g / TOTAL_PIXELS, dtype=tf.float32)
-        l = tf.mul(m, logits) + c 
-        loss = tf.reduce_sum(tf.mul(tf.square(label_placeholder - logits), l), name='loss')
-    """
-
-    #tf.scalar_summary('loss_m', m)
-    #tf.scalar_summary('loss_c', c)
-    #tf.histogram_summary('tmp', tmp)
-    #tf.histogram_summary('lamb', lamb)
-    #tf.histogram_summary('squr', squr)
-    #tf.scalar_summary('loss_l', l)
     # Log data
-    #tf.image_summary("prediction", netTools.tensor2img(logits, BATCH_SIZE))
+    tf.histogram_summary('tmp', tmp)
+    tf.histogram_summary('lamb', lamb)
+    tf.histogram_summary('squr', squr)
     tf.scalar_summary(loss.op.name, loss)
 
     # Training operations
@@ -162,7 +150,7 @@ with tf.Graph().as_default():
             summary_writer.add_summary(summary_str, step)
             
             # Save a checkpoint and evaluate the model periodically.
-            if step % 300 == 0:
+            if step % 1000 == 0:
                 saver.save(sess, SAVE_DIR, global_step=step)
                 
                 # RUN Network with validation data
@@ -176,9 +164,9 @@ with tf.Graph().as_default():
                 preds, validation_value = sess.run([logits, loss], feed_dict=feed_dict)
                 print("Step: %d: validation: %.5f" % (step, validation_value))
                 
-                if step == 30000:
+                if step % 100000 == 0:
                     # Save predictions for viewing later
-                    pic_name = "test" + str(step) 
+                    pic_name = "g3_" + str(step) 
                     netTools.save_preds(batch_data, preds, pic_name, batch_size=BATCH_SIZE)
 
 