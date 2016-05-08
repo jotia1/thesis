339dc83f882aac3013f62bf2793cc3f12a4559b6
Joshua Arnold <joshua.arnold1@uqconnect.edu.au>
2016-05-09 08:19:50 +1000

    Added net3 for historic reasons

---
+++ tf_models/net3.py
@@ -0,0 +1,194 @@
+import numpy as np
+import tensorflow as tf
+import netTools
+import time
+import math
+
+PICKLE_FILE = 'gt5_20k.pickle'
+# TODO need a new pickle file
+#TENSORBOARD_DIR = '/home/Student/s4290365/thesis/tf_models/tensorBoard'
+TENSORBOARD_DIR = 'tensorBoard'
+#SAVE_DIR = '/home/Student/s4290365/thesis/tf_models/saveDir'
+SAVE_DIR = 'saveDir/'
+
+# CONSTANTS
+TRAIN_INDEX = 0
+VALID_INDEX = 1
+TEST_INDEX = 2
+
+BATCH_SIZE = 100
+TOTAL_STEPS = 501
+HIDDEN_UNITS = 16
+LEARNING_RATE = 0.5
+
+# Net variable
+NUM_INPUT_UNITS = 12
+NUM_OUTPUT_UNITS = 12
+
+# Load and separate datasets
+data = netTools.load_pickle_dataset(PICKLE_FILE)
+train_dataset, train_labels = data[TRAIN_INDEX]
+valid_dataset, valid_labels = data[VALID_INDEX]
+test_dataset, test_labels = data[TEST_INDEX]
+
+# Build graph
+# Using the standard graph
+with tf.Graph().as_default():
+    
+    # Network inputs and outputs
+    input_placeholder = tf.placeholder(tf.float32, shape=(BATCH_SIZE,
+                                                        NUM_INPUT_UNITS))
+    label_placeholder = tf.placeholder(tf.float32, shape=(BATCH_SIZE,
+                                                        NUM_OUTPUT_UNITS))
+    
+    # Define hidden layer
+    with tf.name_scope('hidden'):
+        weights = tf.Variable(tf.truncated_normal([NUM_INPUT_UNITS, HIDDEN_UNITS], 
+                                stddev=1.0 / HIDDEN_UNITS),
+                                name='weights')
+    
+        biases = tf.Variable(tf.truncated_normal([HIDDEN_UNITS], 
+                                stddev=1.0 / HIDDEN_UNITS),
+                                name='biases')
+        hidden = tf.nn.relu(tf.matmul(input_placeholder, weights) + biases)
+
+    tf.histogram_summary("hid_biases", biases)
+    tf.histogram_summary("hid_weights", weights)
+    tf.histogram_summary("hidden", hidden)
+
+    # Define output layer
+    with tf.name_scope('out_layer'):
+        weights = tf.Variable(
+            tf.truncated_normal([HIDDEN_UNITS, NUM_OUTPUT_UNITS],
+                                stddev=1.0 / HIDDEN_UNITS),
+            name='weights')
+
+        biases = tf.Variable(tf.truncated_normal([NUM_OUTPUT_UNITS], 
+                                stddev=1.0 / HIDDEN_UNITS),
+                                name='biases')
+        logits = tf.matmul(hidden, weights) + biases
+
+    tf.histogram_summary("out_biases", biases)
+    tf.histogram_summary("out_weights", weights)
+    tf.histogram_summary("logits", logits)
+
+    # loss function - Sum squared difference (well mean...)
+    loss = tf.reduce_sum(tf.square(label_placeholder - logits), name='loss')
+    #loss = tf.reduce_sum(tf.square(label_placeholder - logits), name='loss')
+
+    # Linear Loss - sum( (t - a)^2 * lamb(t) )
+    """
+    with tf.name_scope('loss_layer'):
+        with tf.name_scope('lamb'):
+            # Computer L(actual) = actual * m + c
+            g = float(10) # Grayness (or whiteness) of the scene
+            m = tf.constant( (TOTAL_PIXELS - (2.0 * g)) / TOTAL_PIXELS, dtype=tf.float32 )
+            c = tf.constant( float(g) / TOTAL_PIXELS, dtype=tf.float32)
+            lamb = tf.mul(m, logits) + c
+
+        squr = tf.square(label_placeholder - logits, name='squr')
+        tmp = tf.mul(squr, lamb, name='tmp')
+        loss = tf.reduce_sum(tf.div(tmp + 1e-9, TOTAL_PIXELS), name='loss')
+
+    tf.histogram_summary('tmp', tmp)
+    tf.histogram_summary('lamb', lamb)
+    tf.histogram_summary('squr', squr)
+    """
+    
+    # Partwise loss - sum(relu(l - p) * ~1) + sum(relu(p - l) * ~0)
+    # If it doesn't guess a white highenough its a big loss but if it mislabels
+    # a 0, its not a big deal
+    """
+    with tf.name_scope('loss_layer'):
+        g = float(10)
+        wc = tf.constant( (TOTAL_PIXELS - g) / float(TOTAL_PIXELS) )
+        bc = tf.constant( g / TOTAL_PIXELS )
+        ww = tf.reduce_sum(tf.mul(tf.nn.relu(tf.sub(label_placeholder, logits)), wc)) # Wrong white
+        wb = tf.reduce_sum(tf.mul(tf.nn.relu(tf.sub(logits, label_placeholder)), bc)) # wrong black
+        loss = tf.add(ww, wb, name='loss')
+
+    tf.histogram_summary('ww', ww)
+    tf.histogram_summary('wb', wb)
+    """
+
+    # Log data
+    tf.scalar_summary(loss.op.name, loss)
+
+    # Training operations
+    optimiser = tf.train.GradientDescentOptimizer(LEARNING_RATE)
+    global_step = tf.Variable(0, name='global_step', trainable=False)
+    train_op = optimiser.minimize(loss, global_step=global_step)
+    
+    # Collect all summaries together into one operation
+    summary_op = tf.merge_all_summaries()
+    
+    # Saver will save the state of the network in case of crash etc.
+    saver = tf.train.Saver()
+
+
+    # Run graph
+    sess = tf.Session()
+    
+    # Initialise variables 
+    init = tf.initialize_all_variables()
+    sess.run(init)
+
+    summary_writer = tf.train.SummaryWriter(TENSORBOARD_DIR,
+                                            graph_def=sess.graph_def)
+    
+    for step in xrange(TOTAL_STEPS):
+        start_time = time.time()
+
+        # Create feed dictionary
+        offset = (step * BATCH_SIZE) % (train_labels.shape[0] - BATCH_SIZE)
+        batch_data = train_dataset[offset : (offset + BATCH_SIZE), :]
+        batch_labels = train_labels[offset : (offset + BATCH_SIZE), :]
+        feed_dict = { input_placeholder : batch_data, 
+                    label_placeholder : batch_labels 
+                    }
+
+        """
+        ## DEBUGGING ################
+        # Just compute the loss
+        loss_value = sess.run([loss], feed_dict=feed_dict)
+        # THen write the loss and other stats to file
+        summary_str = sess.run(summary_op, feed_dict=feed_dict)
+        summary_writer.add_summary(summary_str, step)
+
+        continue  # Force loop restart
+        #################    END DEBUG   ################
+        """
+
+        _, loss_value = sess.run([train_op, loss], feed_dict=feed_dict)
+
+        duration = time.time() - start_time
+
+        if step % 10 == 0:
+            summary_str, _ = sess.run([summary_op, lamb], feed_dict=feed_dict)
+            summary_writer.add_summary(summary_str, step)
+            
+            # Save a checkpoint and evaluate the model periodically.
+            if step % 1000 == 0:
+                #saver.save(sess, SAVE_DIR, global_step=step)
+                
+                # RUN Network with validation data
+                offset = (step * BATCH_SIZE) % (valid_labels.shape[0] - BATCH_SIZE)
+                batch_data = valid_dataset[offset : (offset + BATCH_SIZE), :]
+                batch_labels = valid_labels[offset : (offset + BATCH_SIZE), :]
+                feed_dict = { input_placeholder : batch_data, 
+                            label_placeholder : batch_labels 
+                            }
+
+                preds, validation_value, = sess.run([logits, loss], feed_dict=feed_dict)
+                print("Step: %d: validation: %.5f" % (step, validation_value))
+                
+                if False: #$step % 100000 == 0:
+                    # Save predictions for viewing later
+                    pic_name = "g3_" + str(step) 
+                    netTools.save_preds(batch_data, preds, pic_name, batch_size=BATCH_SIZE)
+
+
+
+
+
+
